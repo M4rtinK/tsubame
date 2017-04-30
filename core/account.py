0bdeb6ff5f65edc4a1382ff46516ae75bb3c6d10
Martin Kolman <martin.kolman@gmail.com>
2017-04-30 23:28:58 +0200

    Switch Account to BlitzDB based persistence

--- core/account.py
+++ core/account.py
@@ -18,116 +18,35 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #--------------------------------------------------------------------------
 
-import os
-import glob
-
+import blitzdb
 from core.base import TsubameBase
 
-class Account(TsubameBase):
-    """A generic account base class."""
+class TwitterAccount(blitzdb.Document):
+    """A Twitter account.
+    
+    Expected attributes:    
+    id str: unique account id
+    name str: human readable account name
+    token str: Twitter access token
+    token_secret str: Twitter access token secret
+    """
 
-    def __init__(self, id, name):
-        super(Account, self).__init__()
-        self._id = id
-        self._name = name
-
-    @property
-    def log_prefix(self):
-        return "core.account"
-
-    @property
-    def id(self):
-        """Unique account id.
-        
-        :returns: unique account id
-        :rtype: str
-        """
-        return self._id
-
-    @property
-    def name(self):
-        """User specified account name.
-        
-        :returns: user specified account name
-        :rtype: str
-        """
-        return self._name
-
-
-    def to_dict(self):
-        """Return dictionary representing state of the Account instance."""
-        d = super(Account, self).to_dict()
-        d["id"] = self.id
-        d["name"] = self.name
-        return d
-
-class TwitterAccount(Account):
-    """A Twitter account."""
-
-    @classmethod
-    def from_dict(cls, dictionary):
-        #TODO: validate the dict first
-        return cls(id=dictionary["id"],
-                   name=dictionary["name"],
-                   token=dictionary["token"],
-                   token_secret=dictionary["token_secret"]
-                   )
-
-    def __init__(self, id, name, token, token_secret):
-        super(TwitterAccount, self).__init__(id, name)
-        self._token = token
-        self._token_secret = token_secret
-
-    def __repr__(self):
+    def __str__(self):
         return "%s (%s) - a Twitter account" % (self.id, self.name)
-
-    @property
-    def log_prefix(self):
-        return "core.account.twitter"
-
-    @property
-    def token(self):
-        """Twitter account access token.
-        
-        :returns: Twitter account access token
-        :rtype: str
-        """
-        return self._token
-
-    @property
-    def token_secret(self):
-        """Twitter account access token secret.
-        
-        :returns: Twitter account access token secret
-        :rtype: str
-        """
-        return self._token_secret
-
-    def to_dict(self):
-        """Return dictionary representing state of the TwitterAccount instance."""
-        d = super(TwitterAccount, self).to_dict()
-        d["token"] = self.token
-        d["token_secret"] = self.token_secret
-        return d
 
 class AccountManager(TsubameBase):
     """Service account management."""
 
-    def __init__(self, accounts_folder):
+    def __init__(self, main_db):
         super(AccountManager, self).__init__()
         self._accounts = {}
-        self._accounts_folder = accounts_folder
+        self._main_db = main_db
 
         # load stored accounts (if any)
-        if os.path.isdir(accounts_folder):
-            accounts_glob = os.path.join(accounts_folder, "*.json")
-            account_files = glob.glob(accounts_glob)
-            for file_path in account_files:
-                try:
-                    account = TwitterAccount.from_json_file(file_path)
-                    self._accounts[account.id] = account
-                except Exception:
-                    self.log.exception("account file loading failed for: %s", file_path)
+        accounts = self._main_db.filter(TwitterAccount, {})
+
+        for account in accounts:
+            self._accounts[account.id] = account
 
         # if self._accounts:
         #     if len(self._accounts):
@@ -146,16 +65,18 @@ class AccountManager(TsubameBase):
         return self._accounts
 
     def add(self, account, replace=False):
+        # We could just add the account to the database,
+        # but we want to prevent unwanted account overwrites,
+        # so we use this function and the account manager.
         if account.id in self._accounts.keys() and not replace:
             self.log.error("can't add account %s - already present")
             return False
 
-        # set storage path
-        account_file_path = os.path.join(self._accounts_folder, account.id + ".json")
-        account.json_file_path = account_file_path
+        # add the account to the database
         try:
             # save the account
-            account.save()
+            account.save(self._main_db)
+            self._main_db.commit()
             # add it to tracking
             self._accounts[account.id] = account
             self.log.info("account has been added: %s", account)
@@ -167,7 +88,8 @@ class AccountManager(TsubameBase):
         account = self._accounts.get(account_id)
         if account:
             try:
-                os.remove(account.json_file_path)
+                self._main_db.delete(account)
+                self._main_db.commit()
                 del self._accounts[account_id]
                 self.log.info("account %s has been removed", account_id)
             except Exception:
