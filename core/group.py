ab61fa824b326499d926f1fc685f2c8ff792f5b7
Martin Kolman <martin.kolman@gmail.com>
2017-05-11 00:31:53 +0200

    Make groups and filter groups persistent
    And also add machinery needed to correctly work with nested persistent objects, so that the contained functional class instances are kept in sync with the data class instances.

--- core/group.py
+++ core/group.py
@@ -18,40 +18,56 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #----------------------------------------------------------------------------
 
+import blitzdb
+
 from threading import RLock
 
-from core.base import TsubameBase
+from core.base import TsubamePersistentBase
 from core import filter
 
-class Group(TsubameBase):
+class Group(TsubamePersistentBase):
     """And ordered serializable group of items.
     
     Supports atomic operations on the contained group.
     """
 
-    def __init__(self, items=None):
-        super(Group, self).__init__()
+    data_defaults = {"items" : []}
+
+    def __init__(self, db, data):
+        super(Group, self).__init__(db, data)
         self._group_lock = RLock()
-        if items is None:
-            items = []
-        self._items = []
+        self._items = None
 
     def add(self, item):
         with self._group_lock:
+            # the items to be added need to be
+            # persistent class instances
+            self.data.items.append(item.data)
             self._items.append()
 
     def clear(self):
         with self._group_lock:
+            self.data.items.clear()
             self._items.clear()
+
+    def _get_items(self):
+        raise NotImplementedError
 
     @property
     def items(self):
         with self._group_lock:
+            if self._items is None:
+                self._items = self._get_items()
             return self._items
 
     def pop(self, index):
         with self._group_lock:
+            self.data.items.pop(index)
             return self._items.pop(index)
+
+    # NOTE: The reordering operations need to be always done
+    #       in the backing filter data list as well to keep
+    #       them in sync.
 
     def swap(self, item_1, item_2):
         # TODO
@@ -64,13 +80,33 @@ class Group(TsubameBase):
     def replace_items(self, new_items):
         with self._group_lock:
             self._items.clear()
+            self.data.items.clear()
             self._items = new_items
+            self.data.items = [i.data for i in new_items]
 
+class FilterGroupData(blitzdb.Document):
+    pass
 
 class FilterGroup(Group):
 
-    def __init__(self, items=None):
-        super(FilterGroup, self).__init__(items=items)
+    @classmethod
+    def new(cls, db):
+        data = FilterGroupData(cls.data_defaults)
+        return cls(db, data)
+
+    def _get_items(self):
+        items = []
+        for item_data in self.data.items:
+            # Fetch the functional filter class based
+            # based on data class.
+            cls = filter.CLASS_MAP.get(item_data.__class__)
+            if cls is None:
+                self.log.error("Filter class class not found for data: %s",
+                                item_data)
+            else:
+                items.append(cls(self.db, item_data))
+        return items
+
 
 
 
