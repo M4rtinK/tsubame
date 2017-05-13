23291bf8a3d66ee8c017d34d13921093e86bc60c
Martin Kolman <martin.kolman@gmail.com>
2017-05-13 18:24:32 +0200

    Change the accounts tests for the new account implementation

--- tests/account_test.py
+++ tests/account_test.py
@@ -10,41 +10,32 @@ class TwitterAccountClassTest(unittest.TestCase):
 
     def account_test(self):
         """Check if twitter_accounts can be properly instantiated"""
-        account = TwitterAccount({
-            "username": "avatar",
-            "name": "Steve",
-            "token" : "oxium",
-            "token_secret" : "radium"
-        }
-        )
-        self.assertEquals(account.username, "avatar")
-        self.assertEquals(account.name, "Steve")
-        self.assertEquals(account.token, "oxium")
-        self.assertEquals(account.token_secret, "radium")
+        with tempfile.TemporaryDirectory() as temp_dir_name:
+            db = blitzdb.FileBackend(temp_dir_name)
+
+            account = TwitterAccount.new(db, username="avatar", token="oxium",
+                                         token_secret="radium", name="Steve")
+            self.assertEquals(account.username, "avatar")
+            self.assertEquals(account.name, "Steve")
+            self.assertEquals(account.token, "oxium")
+            self.assertEquals(account.token_secret, "radium")
 
     def serialisation_test(self):
         """Check that twitter_accounts can be serialized and deserialized"""
         with tempfile.TemporaryDirectory() as temp_dir_name:
             db = blitzdb.FileBackend(temp_dir_name)
 
-            account = TwitterAccount(
-                {
-                    "username": "avatar",
-                    "name": "Steve",
-                    "token": "oxium",
-                    "token_secret": "radium"
-                }
-            )
+            account = TwitterAccount.new(db, username="avatar", token="oxium",
+                                         token_secret="radium", name="Steve")
 
             # check that the temporary folder exists (just in case)
             self.assertTrue(os.path.isdir(temp_dir_name))
 
             # serialize the account to the database
-            db.save(account)
-            db.commit()
+            account.save(commit=True)
 
             # deserialize the account from the file
-            loaded_account = db.get(TwitterAccount, {"username": "avatar"})
+            loaded_account = TwitterAccount.from_db(db, username="avatar")
 
             # check that the deserialized account has the expected properties
             self.assertEquals(loaded_account.username, "avatar")
