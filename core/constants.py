f099e079fced3f840d588cbf0212500ea0b87ebe
Martin Kolman <martin.kolman@gmail.com>
2017-05-31 00:21:09 +0200

    Add some initial content to the constants file
    Just device type definitions and platform ids for now.

---
+++ core/constants.py
@@ -0,0 +1,18 @@
+from enum import Enum
+
+# device types
+DEVICE_TYPE_DESKTOP = 1
+DEVICE_TYPE_SMARTPHONE = 2
+DEVICE_TYPE_TABLET = 3
+
+# device ids
+
+
+class DeviceID(Enum):
+    PC = "pc"
+    SAILFISH = "sailfish"
+    ANDROID = "android"
+    N900 = "n900"
+    BB10 = "bb10"
+    N9 = "n9"
+    NEO = "neo"
